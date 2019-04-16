7d88c1e1aa772bb304f5889c99a855c30032a38e
rayanazzam <rahmed@udallas.edu>
2019-04-16 13:24:03 -0500

    Addes test files for GHappyTest class

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,21 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+    @ParameterizedTest
+    @CsvSource({"xgx", "gxx", "xxg"})
+    public void testForUnhappyG(String str) {
+        Assertions.assertFalse(new GHappy().gHappy(str));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"xgg", "ggx","xggx","yggx"})
+    public void testForHappyG(String str) {
+        Assertions.assertTrue(new GHappy().gHappy(str));
+    }
+
 }
